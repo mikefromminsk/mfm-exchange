3faea13e64c7c30b253c58c8eb67e85f8bafd61f
Mike Haiduk <x29a100@gmail.com>
2024-11-17 11:42:51 +0100

    init Chrome extension

--- bot/job.php
+++ bot/job.php
@@ -2,6 +2,7 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "/mfm-data/utils.php";
 
 $gas_domain = get_required(gas_domain);
+$gas_domain = get_required(gas_domain);
 
 foreach (getDomains() as $domain) {
     if ($domain != $gas_domain) {
