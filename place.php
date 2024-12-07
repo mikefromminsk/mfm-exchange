949476bd420183a0d44bfb2ccb070fddacbbe52e
Mike Haiduk <x29a100@gmail.com>
2024-12-07 23:35:25 +0100

    start stake

--- place.php
+++ place.php
@@ -9,6 +9,9 @@ $amount = get_int_required(amount);
 $total = get_int_required(total);
 $pass = get_required(pass);
 
+if (!str_starts_with($address, "bot_spred_"))
+    limitPassSec(1, $domain);
+
 $order_id = place($domain, $address, $is_sell, $price, $amount, $total, $pass);
 
 if ($order_id == null) error("place error");
